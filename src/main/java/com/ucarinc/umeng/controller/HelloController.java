fda92e2781a0b3425883fef9b302d893a46d9342
suanle <anle.su@ucarinc.com>
2020-04-03 16:40:21 +0800

    完善首页概率接口

--- src/main/java/com/ucarinc/umeng/controller/HelloController.java
+++ src/main/java/com/ucarinc/umeng/controller/HelloController.java
@@ -16,6 +16,8 @@ import com.ucarinc.umeng.service.EventProbabilityInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.DecimalFormat;
@@ -35,6 +37,25 @@ public class HelloController {
     public static final String SEC_KEY = "ATZNZYZ6fT";
 
     public static final String SERVER_HOST = "gateway.open.umeng.com";
+
+    public static final String[] eventList = new  String[] {
+            "XQ_App_StartUp",
+            "XQ_App_UserCenter",
+            "XQ_APP_CustomerService",
+           "XQ_ZCN_Home_BannerClick",
+            "XQ_ZCN_Home_PickupCityClick",
+            "XQ_ZCN_Home_PickupAddressClick",
+            "XQ_ZCN_Home_ReturnCityClick",
+            "XQ_ZCN_Home_ReturnAddressClick",
+            "XQ_ZCN_Home_CrossDiffAddressSwitch",
+            "XQ_ZCN_Home_LocationMeClick",
+            "XQ_ZCN_Home_CalendarClick",
+            "XQ_ZCN_Home_GotoBookCarClick",
+            "XQ_ZCN_Home_WeekRentClick",
+            "XQ_ZCN_Home_BottomMoreHitchhikingClick",
+            "XQ_ZCN_Home_BottomHitchhikingClick",
+
+    };
 
     ApiExecutor apiExecutor = new ApiExecutor(APP_KEY, SEC_KEY);
 
@@ -60,23 +81,77 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("/getHomePageEventProbabilit")
     //http://localhost:8080/api/getHomePageEventProbabilit?events=%20XQ_ZCN_Home_BannerClick,%20XQ_App_UserCenter&startDate=2020-04-01&endDate=2020-04-01
-    public String getHomePageEventProbabilit(@RequestParam(value = "events") List<String> events,String startDate, String endDate,@RequestParam(defaultValue = "7.0.0") String version) {
-        if (events.isEmpty()) return "事件名称参数错误，必须是字符串数组";
-        if (startDate.isEmpty() || endDate.isEmpty()) return "开始，截止时间参数错误，必须是（yyyy-MM-dd）格式";
+    //时间 必须是（yyyy-MM-dd）格式
+    //endDate不传 只请求某一个的概率
+    //startDate endDate都有值 则请求这个时间段内的 概率和
+    public String getHomePageEventProbabilit(@RequestParam(value ="events", required = false) String[] events,String startDate, String endDate,@RequestParam(defaultValue = "7.0.0") String version) {
+        if (events == null || events.length == 0) {
+            events = eventList;
+
+        }
+        if (startDate.isEmpty()) return "开始时间参数为空";
+
+
+
 
         List<EventProbabilityInfo> resultArr = new ArrayList<>();
+        if (StringUtils.isEmpty(endDate) || startDate.equals(endDate)) {
+            for (int i = 0; i < events.length; i++) {
+                String eventName = events[i];
+                List<EventProbabilityInfo> tempArr = eventProbabilityInfoService.selectByNameAndDate(eventName, startDate, startDate);
+                if (!tempArr.isEmpty()) {
+                    resultArr.addAll(tempArr);
 
-        for (int i = 0; i < events.size(); i++) {
-            String eventName = events.get(i);
-            List<EventProbabilityInfo> tempArr = eventProbabilityInfoService.selectByNameAndDate(eventName, startDate, endDate);
-            if (!tempArr.isEmpty()) {
-                resultArr.addAll(tempArr);
+                }
+            }
+        } else {
+//            Integer appstartCount = 0;
+//            List<EventProbabilityInfo> appstartInfos = eventProbabilityInfoService.selectByNameAndDate("XQ_App_StartUp", startDate, endDate);
+//            for(EventProbabilityInfo info : appstartInfos) {
+//                appstartCount += info.getCount();
+//            }
+//            DecimalFormat df = new DecimalFormat("0.00");
+//
+//            for (int i = 0; i < events.length; i++) {
+//                String eventName = events[i];
+//                List<EventProbabilityInfo> tempArr = eventProbabilityInfoService.selectByNameAndDate(eventName, startDate, endDate);
+//                if (!tempArr.isEmpty()) {
+//                    Integer tempNum = 0;
+//                    for(EventProbabilityInfo info : tempArr) {
+//                        tempNum += info.getCount();
+//                    }
+//                    EventProbabilityInfo resultInfo = new EventProbabilityInfo();
+//                    resultInfo.setName(eventName);
+//                    resultInfo.setProbabilit(df.format(tempNum /appstartCount));
+//                    resultArr.add(resultInfo);
+//                }
+//            }
+
+            List<EventProbabilityInfo> appstartInfos = eventProbabilityInfoService.selectByNameAndDate("XQ_App_StartUp", startDate, endDate);
+            if(CollectionUtils.isEmpty(appstartInfos)){
+                //分母一般不为零的，如果为零看需要抛出什么一样
+            }
+
+            DecimalFormat df = new DecimalFormat("0.00");
+            Integer sum = appstartInfos.stream().mapToInt(EventProbabilityInfo::getCount).sum();
+            for (int i = 0; i < events.length; i++) {
+                String eventName = events[i];
+                List<EventProbabilityInfo> tempArr = eventProbabilityInfoService.selectByNameAndDate(eventName, startDate, endDate);
+                if (!tempArr.isEmpty()) {
+                    Integer sum1 = tempArr.stream().mapToInt(EventProbabilityInfo::getCount).sum();
+                    EventProbabilityInfo resultInfo = new EventProbabilityInfo();
+                    resultInfo.setName(eventName);
+                    resultInfo.setCount(sum1);
+                    resultInfo.setProbabilit(df.format(sum1 /sum));
+                    resultArr.add(resultInfo);
+                }
             }
         }
 
 
-        JSONArray jsonArray = new JSONArray(Collections.singletonList(resultArr));
-        return jsonArray.toJSONString();
+
+        //对象数组转json
+        return JSON.toJSONString(resultArr);
     }
 
     @ResponseBody
@@ -84,34 +159,39 @@ public class HelloController {
     //根据版本号 时间段获取所有事件列表
     public String eventList(String startDate, String endDate,@RequestParam(defaultValue = "7.0.0") String version, Integer page){
         configApiExecutor();
-        String resultStr = UMengRequestCommon.umengUappEventList(apiExecutor,startDate,endDate,version,page);
-        JSONObject jsonObject = JSONObject.parseObject(resultStr);//字符串转json对象
-        String data = jsonObject.getString("eventInfo");
-        List<EventInfo> list=JSONObject.parseArray(data, EventInfo.class);
+        //数据量太大
+//        String resultStr = UMengRequestCommon.umengUappEventList(apiExecutor,startDate,endDate,version,page);
+//        JSONObject jsonObject = JSONObject.parseObject(resultStr);//字符串转json对象
+//        String data = jsonObject.getString("eventInfo");
+//        List<EventInfo> list=JSONObject.parseArray(data, EventInfo.class);
+//
+////        Boolean deleteResult = eventInfoService.deleteAll();
+////        logger.info("删除事件表" + deleteResult);
+//
+//        if(eventInfoService.insertEventInfo(list)){
+//            logger.info("数据入库成功");
+//        }else{
+//            logger.info("数据入库失败");
+//        }
 
-//        Boolean deleteResult = eventInfoService.deleteAll();
-//        logger.info("删除事件表" + deleteResult);
+        //只请求首页的所有事件数据
 
-        if(eventInfoService.insertEventInfo(list)){
-            logger.info("数据入库成功");
-        }else{
-            logger.info("数据入库失败");
-        }
+
 
         List<EventProbabilityInfo> probabilityInfos = new ArrayList<>();
         List<DateCountInfo> startUpInfos = new ArrayList<>();
 
         //遍历事件列表 请求一个时间段内该事件的统计数据
-        for(int i = 0; i < list.size(); i++) {
-            EventInfo eventInfo = list.get(i);
+        for(int i = 0; i < eventList.length; i++) {
+            String eventName = eventList[i];
 
 
             //筛选出APP启动次数的数据
-            if (eventInfo.getName().equals("XQ_App_StartUp")) {
-                startUpInfos = eventDataFun(eventInfo.getName(), startDate, endDate);
+            if (eventName.equals("XQ_App_StartUp")) {
+                startUpInfos = eventDataFun(eventName, startDate, endDate);
 
             } else {
-                eventDataFun(eventInfo.getName(), startDate, endDate);
+                eventDataFun(eventName, startDate, endDate);
             }
         }
 
@@ -123,17 +203,20 @@ public class HelloController {
             DateCountInfo startUpInfo = startUpInfos.get(i);
             //根据日期 取到该日期下的所有统计数据
             currentDateInfos = dateCountInfoService.selectDateCountInfo(startUpInfo.getDate());
-            for (int j = 0;j < currentDateInfos.size();j++) {
+            if (!currentDateInfos.isEmpty()) {
+                for (int j = 0;j < currentDateInfos.size();j++) {
 
-                //遍历每一条数据 计算概率
-                DateCountInfo dateCountInfo = currentDateInfos.get(j);
-                EventProbabilityInfo eventProbabilityInfo = new EventProbabilityInfo();
-                eventProbabilityInfo.setCount(dateCountInfo.getCount());
-                eventProbabilityInfo.setDate(dateCountInfo.getDate());
-                eventProbabilityInfo.setName(dateCountInfo.getName());
-                eventProbabilityInfo.setProbabilit(df.format( (double)dateCountInfo.getCount() / startUpInfo.getCount()));
-                probabilityInfos.add(eventProbabilityInfo);
+                    //遍历每一条数据 计算概率
+                    DateCountInfo dateCountInfo = currentDateInfos.get(j);
+                    EventProbabilityInfo eventProbabilityInfo = new EventProbabilityInfo();
+                    eventProbabilityInfo.setCount(dateCountInfo.getCount());
+                    eventProbabilityInfo.setDate(dateCountInfo.getDate());
+                    eventProbabilityInfo.setName(dateCountInfo.getName());
+                    eventProbabilityInfo.setProbabilit(df.format( (double)dateCountInfo.getCount() / startUpInfo.getCount()));
+                    probabilityInfos.add(eventProbabilityInfo);
+                }
             }
+
         }
 
         logger.info("概率计算完成");
@@ -143,7 +226,7 @@ public class HelloController {
             logger.info("概率数据入库失败");
         }
 
-        return resultStr;
+        return JSON.toJSONString(probabilityInfos);
     }
 
     @ResponseBody
